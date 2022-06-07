c419201b0a073f267deaa6e604dd2f27c05e4455
GHOSt <ghostdevbusiness@gmail.com>
2022-06-07 16:45:54 +0100

    test another package

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+    // import { Hamburger } from 'svelte-hamburgers'
     import { copy } from 'svelte-copy'
 </script>
 
+<Hamburger />
+<!-- 
 <button use:copy={"something or other"}>
     Copy something
-</button>
+</button> -->
