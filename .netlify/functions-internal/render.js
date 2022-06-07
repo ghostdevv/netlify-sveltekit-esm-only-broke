3cac4db25ce91dab94cb1140b63cda9a78f6ae7f
GHOSt <ghostdevbusiness@gmail.com>
2022-06-07 16:41:43 +0100

    init

---
+++ .netlify/functions-internal/render.js
@@ -0,0 +1,32 @@
+const { init } = require('../serverless.js');
+
+exports.handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"start-f1c54ed5.js","js":["start-f1c54ed5.js","chunks/index-6cff4357.js"],"css":[]},
+		nodes: [
+			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				type: 'page',
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				path: "/",
+				shadow: null,
+				a: [0,2],
+				b: [1]
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { copy } from 'svelte-copy'
+</script>
+
+<button use:copy={"something or other"}>
+    Copy something
+</button>
